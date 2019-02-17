3cd8cebeeecdc9f6e3713a3654af8f7e44e37408
Arisylia <eh.jay.cee@gmail.com>
2019-02-17 11:35:59 -0500

    no gp

--- analyzeImageReqNoGP.py
+++ analyzeImageReqNoGP.py
@@ -4,32 +4,10 @@ import os
 import json
 import threading
 import time
-import mraa
-from subprocess import Popen, PIPE
-import GPIOLibrary
-
-try:
-    Pin23 = GP.getPin23()
-    Pin23.out()
-
-    Pin24 = GP.getPin24()
-    Pin24.out()	
-
-    for i in range(0,20):
-        pinValue = Pin29.getValue()
-	
-        if pinValue == 1:
-            Pin27.high()
-        else:
-            Pin27.low()
-        time.sleep(1)
-
-finally:
-    GP.cleanup()
 
 url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=dbb6c4cd-454f-4907-9dd3-9c14fbd02d92"
 headers = {"prediction-Key": 'f24c0306b1ac4e0784ac5566960105de', "Content-Type": "application/octet-stream"}
-print(mraa.getVersion())
+
 
 def capture():
     #subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
@@ -47,25 +25,15 @@ def getParkingStatusFromJson(jsonData):
     for i in data['predictions']:
         print(i['probability'])
         if i['probability'] >= 0.20:
-            return True
-    return False
+            return "Empty Spot Available"
+    return "No Spots :("
 
-
-def controlLed(isEmpty):
-    led = [mraa.Gpio(24), mraa.Gpio(25)]
-    for x in led:
-        x.dir(mraa.DIR_OUT)
-        x.write(0)
-        if (isEmpty):
-            led.write(1)
-        else:
-            led.write(0)
 
 def runCapture(n):
-    controlLed(capture())
+    print(capture())
     for i in range(0, n-1):
         time.sleep(15)
-        controlLed(capture())
+        print(capture())
 
 
 runCapture(5)
