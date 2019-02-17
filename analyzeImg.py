45337f12423434dd72ed4d317db078e2aa34866d
Arisylia <eh.jay.cee@gmail.com>
2019-02-17 02:45:55 -0500

    h

--- analyzeImg.py
+++ analyzeImg.py
@@ -1,4 +1,5 @@
-import urllib.request
+
+import urllib
 import subprocess
 import base64
 
@@ -9,24 +10,13 @@ headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type":
 
 def capture():
     subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
-<<<<<<< HEAD
     with open("output.jpg", "rb") as img:
         encodedImg = base64.b64decode(img.read());
     data = urllib.urlencode({"image": encodedImg})
     req = urllib.Request(url, data, headers)
     response = urllib.urlopen(req)
-=======
-    files = {}
-    with open('../Documents/output.jpg', 'rb') as fin:
-        img = fin.read()
-    files['file'] = img
-
-    #img = open('../Documents/output.jpg', 'rb')
-    #files = {'file': img}
-    req = urllib.request.Request(url, files, headers)
-    response = urllib.request.urlopen(req)
->>>>>>> 10e2ab2b565ef6c3066fbf6b174f7f1d94f2e24f
     output = response.read()
     print(output)
+
 
 capture()
