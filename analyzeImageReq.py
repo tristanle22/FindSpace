9e039b2cf349ce2ab4f92155376eb0bfd6842f48
Tristan Le <tristan.le@ssimwave.com>
2019-02-17 07:38:06 -0500

    Automatically answer prompt

--- analyzeImageReq.py
+++ analyzeImageReq.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 import json
 import threading
-
+from subprocess import Popen, PIPE
 url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=dbb6c4cd-454f-4907-9dd3-9c14fbd02d92"
 headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
 
@@ -11,7 +11,8 @@ signal = False
 def capture():
     #subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
     os.system('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
-    os.system('y')
+    p = os.popen(command, "w")
+    p.write("y\n")
     image = open("output.jpg", "rb")
     resp = requests.post(url, headers=headers, data=image)
     output = resp.text
