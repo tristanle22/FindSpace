1e46404face8b038b79a7458e4c2b208ca335be8
tristanle22 <tristan.tuanle@gmail.com>
2019-02-17 08:14:30 +0000

    Conflicts resolved

--- analyzeImg.py
+++ analyzeImg.py
@@ -11,19 +11,11 @@ headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type":
 
 def capture():
     subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
-<<<<<<< HEAD
-    with open("../Documents/output.jpg", "rb") as img:
-        encodedImg = base64.b64decode(img.read());
-    data = urllib.parse.urlencode({"image": encodedImg})
-    req = urllib.request.Request(url, data, headers)
-    response = urllib.request.urlopen(req)
-=======
     with open("output.jpg", "rb") as img:
         encodedImg = base64.b64decode(img.read())
     data = urllib.parse.urlencode({"image": encodedImg})
     req = urllib.request.Request(url, data, headers)
     response = urllib.urlopen(bytes(req))
->>>>>>> 252353b03cb4066980bba664c5414b33a2b0d5f7
     output = response.read()
     print(output)
 
