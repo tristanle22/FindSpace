2f7f529fa3888bb44cf34a879aa9c3afb6619874
Arisylia <eh.jay.cee@gmail.com>
2019-02-17 08:24:27 -0500

    hi

--- analyzeImageReq.py
+++ analyzeImageReq.py
@@ -8,7 +8,7 @@ from subprocess import Popen, PIPE
 url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=dbb6c4cd-454f-4907-9dd3-9c14fbd02d92"
 headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
 
-signal = False
+
 def capture():
     #subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
     #os.system('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
@@ -17,10 +17,10 @@ def capture():
     image = open("output.jpg", "rb")
     resp = requests.post(url, headers=headers, data=image)
     output = resp.text
-    setSignalStatus(parseJson(output))
-    print(signal)
+    return getParkingStatusFromJson(output)
 
-def parseJson(jsonData):
+
+def getParkingStatusFromJson(jsonData):
     data = json.loads(jsonData)
     for i in data['predictions']:
         print(i['probability'])
@@ -28,12 +28,10 @@ def parseJson(jsonData):
             return True
     return False
 
-def setSignalStatus(state):
-    signal = state
 
 def runCapture():
     for i in range(0, 3):
-        capture()
+        print(capture())
         time.sleep(15)
 
 runCapture()
