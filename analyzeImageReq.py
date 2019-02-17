f269b036d3b9c47d6ee5e28b8d78d56cb0c661b1
Arisylia <eh.jay.cee@gmail.com>
2019-02-17 08:49:38 -0500

    assembled with light control

--- analyzeImageReq.py
+++ analyzeImageReq.py
@@ -4,10 +4,11 @@ import os
 import json
 import threading
 import time
+import mraa
 from subprocess import Popen, PIPE
 url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=dbb6c4cd-454f-4907-9dd3-9c14fbd02d92"
 headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
-
+print(mraa.getVersion())
 
 def capture():
     #subprocess.run('ffmpeg -i /dev/video0 -frames 1 ./output.jpg')
@@ -29,10 +30,21 @@ def getParkingStatusFromJson(jsonData):
     return False
 
 
-def runCapture():
-    for i in range(0, 3):
-        print(capture())
+def controlLed(isEmpty):
+    led = [mraa.Gpio(17), mraa.Gpio(27), mraa.Gpio(37)]
+    for x in led:
+        x.dir(mraa.DIR_OUT)
+        x.write(0)
+        if (isEmpty):
+            led.write(1)
+        else:
+            led.write(0)
+
+def runCapture(n):
+    controlLed(capture())
+    for i in range(0, n-1):
         time.sleep(15)
+        controlLed(capture())
 
-runCapture()
 
+runCapture(5);
