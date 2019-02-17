164b9704fb41c922d6ce43240015fe923bdf3775
Arisylia <eh.jay.cee@gmail.com>
2019-02-17 01:45:42 -0500

    newest script

--- analyzeImage.py
+++ analyzeImage.py
@@ -1,8 +1,4 @@
 import urllib
-<<<<<<< HEAD
-=======
-import threading
->>>>>>> 8aa9f7fb603243ac6b0cf108f51bde85d75e02cc
 import pygame
 import pygame.camera
 
@@ -10,17 +6,7 @@ url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Pred
       "0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=8115342a-7571-4f99-bd30-35a2a93521a4"
 headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
 
-<<<<<<< HEAD
 
-def capture():
-    cam = startCamera()
-    img = cam.get_image()
-    files = {'file': img}
-    req = urllib.request.Request(url, files, headers)
-    response = urllib.request.urlopen(req)
-    output = response.read()
-    print(output)
-=======
 """class detectParkingSpot():
     url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/" \
           "0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=8115342a-7571-4f99-bd30-35a2a93521a4"
@@ -58,26 +44,26 @@ url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Pred
     "0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=8115342a-7571-4f99-bd30-35a2a93521a4"
 headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
 
-def main():
+def capture():
     cam = startCamera()
     img = cam.get_image()
-    files = {'file': open(img, 'rb')}
+    files = {'file': img}
     req = urllib.Request(url, files, headers)
     response = urllib.urlopen(req)
     output = response.read()
+    print(output)
 
-    print('output')
 
 def startCamera():
     pygame.init()
     pygame.camera.init()
-    cam = pygame.camera.Camera('/dev/video0')
+    camlist = pygame.camera.list_cameras()
+    if camlist:
+        cam = pygame.caemra.Camera(camlist[0], (640, 480))
+    else:
+        cam = pygame.camera.Camera('/dev/video0')
     cam.start()
     return cam
-
-if __name__ == "__name__":
-    main()
->>>>>>> 8aa9f7fb603243ac6b0cf108f51bde85d75e02cc
 
 
 capture()
