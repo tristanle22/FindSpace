179e9ff86e05e792fbb5b455401d525d3d031d5e
Arisylia <eh.jay.cee@gmail.com>
2019-02-16 23:26:52 -0500

    fk git

---
+++ analyzeImage.py
@@ -0,0 +1,32 @@
+import urllib.request
+import threading
+import pygame
+import time
+import logging
+
+
+class detectParkingSpot():
+    url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/" \
+          "0a95ed60-d8e4-4e5e-998a-787d8021e776/image?iterationId=8115342a-7571-4f99-bd30-35a2a93521a4"
+    headers = {"prediction-Key": '88724618479e49e4939ac9548e804782', "Content-Type": "application/octet-stream"}
+
+    def main(self):
+        cam = self.startCamera()
+        img = cam.get_image()
+        files = {'file': open(img, 'rb')}
+        req = urllib.request.Request(self.url, files, self.headers)
+        response = urllib.request.urlopen(req)
+        output = response.read()
+
+        print('output')
+
+    def startCamera(self):
+        pygame.init()
+        pygame.camera.init()
+        cam = pygame.camera.Camera('/dev/video0')
+        cam.start()
+        return cam
+
+
+
+
